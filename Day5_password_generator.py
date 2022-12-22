9cf6af59d70a76777e81ee889c0cabc7c32906d7
Alex Marks <alexandralmarks@gmail.com>
2022-12-21 21:58:02 -0500

    Day 5 password generator

---
+++ Day5_password_generator.py
@@ -0,0 +1,27 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+chars = [letters, numbers, symbols]
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+order = []
+for i in range(nr_letters):
+    order.append(0)
+for i in range(nr_numbers):
+    order.append(1)
+for i in range(nr_symbols):
+    order.append(2)
+
+random.shuffle(order)
+for o in order:
+    p = random.randint(0,len(chars[o])-1)
+    password = password + chars[o][p]
+
+print("Your new password is " + password)
