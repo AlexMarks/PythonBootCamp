61d6caf940458e904e519082c80636d6052f90e1
Alex Marks <alexandralmarks@gmail.com>
2023-01-10 19:27:05 -0500

    Day 8 code clean-up

--- Day8_caesar_cipher.py
+++ Day8_caesar_cipher.py
@@ -2,27 +2,19 @@
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encode(message, shift):
+def caesar(message, shift, direction):
     new_message = ""
     for l in message:
         try:
             i = letters.index(l)
-            new_message += letters[(i + shift) % 26]
+            if (direction == "encode"):
+                new_message += letters[(i + shift) % 26]
+            elif(direction == "decode"):
+                new_message += letters[(i - shift) % 26]
         except ValueError:
             new_message += l
 
     return new_message
-
-def decode(message, shift):
-    old_message = ""
-    for l in message:
-        try:
-            i = letters.index(l)
-            old_message += letters[(i - shift) % 26]
-        except ValueError:
-            old_message += l
-
-    return old_message
 
 print("""           
  ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
@@ -44,24 +36,17 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
 cmd = ""
 while cmd != "exit":
     cmd = input("Type 'encode', 'decode', or 'exit': ")
-    if cmd == "encode":
-        message = input("Enter the message that you want to encode: ").lower()
+    if cmd == "encode" or cmd == "decode":
+        message = input("Enter the message that you want to use: ").lower()
         shift = input("Enter the number to shift by: ")
         try:
             shift = int(shift) % 26
         except:
             print("Invalid value entered for shift. Please enter a numeric value next time.")
             continue
-        result = encode(message, shift)
-        print(f"Your encoded message now looks like: {result}")
+        result = caesar(message, shift, cmd)
+        print(f"Your message now looks like: {result}")
+    elif cmd != "exit":
+        print("Please enter a valid command from the list.")
 
-    elif cmd == "decode":
-        message = input("Enter the message that you want to decode: ").lower()
-        shift = input("Enter the number to shift by: ")
-        try:
-            shift = int(shift) % 26
-        except:
-            print("Invalid value entered for shift. Please enter a numeric value next time.")
-            continue
-        result = decode(message, shift)
-        print(f"Your decoded message now looks like: {result}")
+
