888e7149b6deec2ecdd533b395975a6591295db4
Alex Marks <16806788+AlexMarks@users.noreply.github.com>
2023-01-10 19:41:38 -0500

    reorganize to submit after fixing git email setting

--- Day8_caesar_cipher.py
+++ Day8_caesar_cipher.py
@@ -1,21 +1,3 @@
-
-
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-def caesar(message, shift, direction):
-    new_message = ""
-    for l in message:
-        try:
-            i = letters.index(l)
-            if (direction == "encode"):
-                new_message += letters[(i + shift) % 26]
-            elif(direction == "decode"):
-                new_message += letters[(i - shift) % 26]
-        except ValueError:
-            new_message += l
-
-    return new_message
-
 print("""           
  ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
 a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
@@ -33,6 +15,23 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
               88                                             
               88           
 """)
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(message, shift, direction):
+    new_message = ""
+    for l in message:
+        try:
+            i = letters.index(l)
+            if (direction == "encode"):
+                new_message += letters[(i + shift) % 26]
+            elif(direction == "decode"):
+                new_message += letters[(i - shift) % 26]
+        except ValueError:
+            new_message += l
+
+    return new_message
+
 cmd = ""
 while cmd != "exit":
     cmd = input("Type 'encode', 'decode', or 'exit': ")
