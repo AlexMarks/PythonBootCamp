3f07007d30811d41b6c548a67773a565314f8171
Alex Marks <16806788+AlexMarks@users.noreply.github.com>
2023-02-07 23:01:07 -0500

    Day 10 functions input and output

---
+++ Day10_Calculator.py
@@ -0,0 +1,50 @@
+#Utilizes input and output of functions
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {"+":add, "-":subtract, "*":multiply, "/":divide}
+
+num1 = int(input("What's the first number?: "))
+
+while True:
+    operator = input("What is the operation? (+, -, *, /, end): ")
+    if operator == "end":
+        break
+    num2 = int(input("What's the second number?: "))
+
+    if operator in operations:
+        answer = operations[operator](num1, num2)
+        print(f"{num1} {operator} {num2} = {answer}")
+        num1 = answer
+    else:
+        print(f"Invalid operator. Reverting to previous answer of {num1}")
+
+
